b4bd39108daf281630685c0ae3abc7ac5b9fc851
Bowenbee <bowenbee@me.com>
2024-05-18 08:58:56 -0700

    Added Solution files for v1.0

---
+++ ProvisionHolidayList.ps1
@@ -0,0 +1,47 @@
+# Provision the Primary List
+
+$SharePointSite = "https://powerfxhelp.sharepoint.com/sites/Sandbox"
+$ListName = "Holidays"
+$HolidayCsv = Import-Csv .\Holidays.csv
+$HolidayChoices = @("Company","Federal")
+
+Connect-PnPOnline -url $SharePointSite -Interactive
+
+
+$CheckExisting =  Get-PnPList -Identity $ListName
+
+If($CheckExisting){
+
+    Remove-PnPList -Identity $ListName -Force
+
+}
+
+New-PnPList -Title $ListName -Template GenericList -OnQuickLaunch:$true
+
+Set-PnPList -Identity $ListName -Description "Holiday List for Date Table usage"
+
+# Add fields
+Add-PnPField -List $ListName -InternalName "MonthNum" -DisplayName "MonthNum" -Type Number -AddToDefaultView
+Add-PnPField -List $ListName -InternalName "Day" -DisplayName "Day" -Type Number -AddToDefaultView
+Add-PnPField -List $ListName -InternalName "WeekNum" -DisplayName "WeekNum" -Type Number -AddToDefaultView
+Add-PnPField -List $ListName -InternalName "DayofWeekNum" -DisplayName "DayofWeekNum" -Type Number -AddToDefaultView
+Add-PnPField -List $ListName -InternalName "HolidayType" -DisplayName "HolidayType" -Type Choice -Choices $HolidayChoices -AddToDefaultView
+Add-PnPField -List $ListName -InternalName "Notes" -DisplayName "Notes" -Type Note -AddToDefaultView
+Set-PnPField -List $ListName "WeekNum" -Values @{Description = "The week number of the month in which the holiday occurs."}
+Set-PnPField -List $ListName "DayofWeekNum" -Values @{Description = "The numerical day of the week the holiday occurs as represented by excel weekday function. If the day varies, leave blank."}
+
+Foreach ($holiday in $HolidayCsv){
+
+    $HolidayObj = @{
+        Title = $holiday.Title
+        MonthNum = $holiday.MonthNum
+        WeekNum = $holiday.WeekNum
+        Day = $holiday.Day
+        DayofWeekNum = $holiday.DayofWeekNum
+        HolidayType = $holiday.HolidayType
+        Notes = $holiday.Notes
+
+    }
+
+    Add-PnPListItem -List $ListName -Values $HolidayObj
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# DateGenerator
+# Date Management and Holiday Tracking Application
+
+## Overview
+This PowerApps Canvas App solution allows users to generate a list of dates based on a specified date range, manage federal and custom holidays, determine actual dates for variable holidays, and export the data to CSV and SharePoint List. This allows the user to easily generate a present and future list of dates for future holidays to use in business date calcuates as a date table.
+
+## Features
+- **Date Range Input**: Input a start and end date to generate a list of all dates within the range.
+- **Holiday Management**: Manage predefined federal holidays and custom holidays, including recurring rules.
+- **Variable Holiday Calculation**: Determine the actual date for holidays that occur on variable dates (e.g., 3rd Monday of January).
+- **Data Export**: Export the date range and holiday information to CSV and SharePoint.
