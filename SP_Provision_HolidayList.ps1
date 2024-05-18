3df50b43d6adf777e0a915266d04b946821c4241
Bowenbee <bowenbee@me.com>
2024-05-18 12:30:13 -0700

    Added new PS Scripts for provisioning lists

--- SP_Provision_HolidayList.ps1
+++ SP_Provision_HolidayList.ps1
@@ -1,9 +1,8 @@
-# Provision the Primary List
+# Provision the Date Generation List
 
 $SharePointSite = "https://powerfxhelp.sharepoint.com/sites/Sandbox"
 $ListName = "Holidays"
 $HolidayCsv = Import-Csv .\Holidays.csv
-$HolidayChoices = @("Company","Federal")
 
 Connect-PnPOnline -url $SharePointSite -Interactive
 
@@ -21,11 +20,12 @@ New-PnPList -Title $ListName -Template GenericList -OnQuickLaunch:$true
 Set-PnPList -Identity $ListName -Description "Holiday List for Date Table usage"
 
 # Add fields
+
 Add-PnPField -List $ListName -InternalName "MonthNum" -DisplayName "MonthNum" -Type Number -AddToDefaultView
 Add-PnPField -List $ListName -InternalName "Day" -DisplayName "Day" -Type Number -AddToDefaultView
 Add-PnPField -List $ListName -InternalName "WeekNum" -DisplayName "WeekNum" -Type Number -AddToDefaultView
 Add-PnPField -List $ListName -InternalName "DayofWeekNum" -DisplayName "DayofWeekNum" -Type Number -AddToDefaultView
-Add-PnPField -List $ListName -InternalName "HolidayType" -DisplayName "HolidayType" -Type Choice -Choices $HolidayChoices -AddToDefaultView
+Add-PnPField -List $ListName -InternalName "HolidayType" -DisplayName "HolidayType" -Type Text -AddToDefaultView
 Add-PnPField -List $ListName -InternalName "Notes" -DisplayName "Notes" -Type Note -AddToDefaultView
 Set-PnPField -List $ListName "WeekNum" -Values @{Description = "The week number of the month in which the holiday occurs."}
 Set-PnPField -List $ListName "DayofWeekNum" -Values @{Description = "The numerical day of the week the holiday occurs as represented by excel weekday function. If the day varies, leave blank."}
@@ -45,3 +45,5 @@ Foreach ($holiday in $HolidayCsv){
 
     Add-PnPListItem -List $ListName -Values $HolidayObj
 }
+
+Disconnect-PnPOnline
